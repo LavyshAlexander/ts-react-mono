589c5846ac5356b7491e7a0da6054ecc2f785fec
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-12-12 21:11:49 +0300

    Added App component.

---
+++ packages/app/src/components/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+
+
+export const App: FC = () => {
+	return (
+		<h1>Here goes the sun.</h1>
+	)
+}
--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './components'
 
 
 ReactDOM.render(
-	<div>Here goes the sun.</div>,
+	<App />,
 	document.getElementById('root')
 )
